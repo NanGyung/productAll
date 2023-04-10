38500b2e9d1a2984a1f0ae8f807ea86297867a9a
“NanGyung” <rhrnakaktdidrod0926@gmail.com>
2023-04-10 11:14:46 +0900

    DAO Test 추가

--- src/test/java/com/kh/product/dao/ProductDAOImplTest.java
+++ src/test/java/com/kh/product/dao/ProductDAOImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -15,33 +16,69 @@ public class ProductDAOImplTest {
   @Autowired
   ProductDAO productDAO;
 
+//  등록
+  @Test
+  @DisplayName("상품등록")
+  void save(){
+    Product product = new Product();
+    product.setPname("딸기");
+    product.setQuantity(10L);
+    product.setPrice(30000L);
+    Long pid = productDAO.save(product);
+    log.info("pid={}",pid);
+    Assertions.assertThat(pid).isGreaterThan(0L);
+  }
+
 //  조회
   @Test
   @DisplayName("상품조회")
   void findById(){
-    Long pid = 15L;
+    Long pid = 62L;
     Optional<Product> product = productDAO.findById(pid);
     Product findedProduct = product.orElseThrow();
-    Assertions.assertThat(findedProduct.getPname()).isEqualTo("자전수정");
-    Assertions.assertThat(findedProduct.getQuantity()).isEqualTo(20L);
-    Assertions.assertThat(findedProduct.getPrice()).isEqualTo(15000L);
+    log.info("product={}",product);
+    Assertions.assertThat(findedProduct.getPname()).isEqualTo("딸기");
+    Assertions.assertThat(findedProduct.getQuantity()).isEqualTo(10L);
+    Assertions.assertThat(findedProduct.getPrice()).isEqualTo(30000L);
   }
 
   //수정
   @Test
   @DisplayName("상품수정")
   void update() {
-    Long pid = 15L;
+    Long pid = 62L;
     Product product = new Product();
-    product.setPname("자전거_수정");
+
+    product.setPname("딸기_수정");
     product.setQuantity(20L);
-    product.setPrice(2000L);
+    product.setPrice(20000L);
     int updatedRowCount = productDAO.update(pid, product);
     Optional<Product> findedProduct = productDAO.findById(pid);
+
+    log.info("product={}",product);
 
     Assertions.assertThat(updatedRowCount).isEqualTo(1);
     Assertions.assertThat(findedProduct.get().getPname()).isEqualTo(product.getPname());
     Assertions.assertThat(findedProduct.get().getQuantity()).isEqualTo(product.getQuantity());
     Assertions.assertThat(findedProduct.get().getPrice()).isEqualTo(product.getPrice());
   }
+
+  // 삭제
+  @Test
+  @DisplayName("상품삭제")
+  void delete(){
+    Long pid = 45L;
+    int columnOfCnt = productDAO.delete(pid);
+    Assertions.assertThat(columnOfCnt).isEqualTo(1);
+  }
+
+  // 목록
+  @Test
+  @DisplayName("상품목록")
+  void findAll(){
+    List<Product> list = productDAO.findAll();
+    log.info("list={}",list);
+    Assertions.assertThat(list.size()).isGreaterThan(0);
+  }
+
 }
